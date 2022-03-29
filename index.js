900d4912cbbde04132e3f250607fdcc550516d03
johnsey <johnsey.reynolds@gmail.com>
2022-03-29 08:24:13 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(){
+    return toExist
+}
+function subtract(){
+    return toExist
+}
+function multiply(){
+    return toExist
+}
+function divide(){
+    return toExist
+}
+function add(a, b){
+    const toEqual = a + b
+    return toEqual
+}
+function subtract(a, b){
+    const toEqual = a - b
+    return toEqual
+}
+function multiply(a, b){
+    const toEqual = a * b
+    return toEqual
+}
+function divide(a, b){
+    const toEqual = a / b
+    return toEqual
+}
+function increment(a){
+    return a += 1
+}
+function decrement(a){
+    return a -= 1
+}
+function makeInt(string){
+    return parseInt(string,10)
+}
+function preserveDecimal(string){
+    return parseFloat(string)
+}
